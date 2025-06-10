f08482fb316920bdd9338413ed7a2597a231564f
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-10 12:10:30 -0700

    Frontend: Add input state

--- src/components/03-Input.jsx
+++ src/components/03-Input.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 export default function Input() {
-  return (
-    <div className='container'>
-      <h2>Input</h2>
-      <input type="text" />
-      <h3>The value inside the input is...</h3>
-    </div>
-  )
+    const [input, setInput] = useState('')
+
+    return (
+        <div className="container">
+            <h2>Input</h2>
+            <input type="text" value={input} onChange={e => {setInput(e.target.value)}}/>
+            <h3>The value inside the input is... {input}</h3>
+        </div>
+    );
 }

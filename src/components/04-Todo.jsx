3a01d49c2b711d7e9770c07e629eea723da10fe8
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-10 12:10:40 -0700

    Frontend: add todo state

--- src/components/04-Todo.jsx
+++ src/components/04-Todo.jsx
@@ -1,28 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const todos = [
-  { id: 1, label: "Refactor the refactor of the refactor", completed: false },
-  { id: 2, label: "Convince rubber duck I'm not the problem", completed: false },
-  { id: 3, label: "Rename variables to something less shameful", completed: false },
-  { id: 4, label: "Google how to center a div (again)", completed: false },
-  { id: 5, label: "Add one more console.log for good luck", completed: false }
-]
+    { id: 1, label: "Refactor the refactor of the refactor", completed: false },
+    {
+        id: 2,
+        label: "Convince rubber duck I'm not the problem",
+        completed: false,
+    },
+    {
+        id: 3,
+        label: "Rename variables to something less shameful",
+        completed: false,
+    },
+    { id: 4, label: "Google how to center a div (again)", completed: false },
+    {
+        id: 5,
+        label: "Add one more console.log for good luck",
+        completed: false,
+    },
+];
 
 export default function Todo() {
-  return (
-    <div className="container">
-      <h2>Todo</h2>
-      <div>
-        {
-          todos.map(todo => {
-            return (
-              <ul key={todo.id}>
-                <li>todo label <button>Done!</button></li>
-              </ul>
-            )
-          })
-        }
-      </div>
-    </div>
-  )
+    const [userTodos, setUserTodos] = useState(todos);
+
+    const toggleItem = (id) => {
+        setUserTodos(userTodos.filter(x => x.id !== id));
+    };
+
+    return (
+        <div className="container">
+            <h2>Todo</h2>
+            <div>
+                {userTodos.map((todo) => {
+                    return (
+                        <ul key={todo.id}>
+                            <li>
+                                {todo.label}{" "}
+                                <button
+                                    onClick={(e) => {
+                                        toggleItem(todo.id);
+                                    }}
+                                >
+                                    {"Done!"}
+                                </button>
+                            </li>
+                        </ul>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }

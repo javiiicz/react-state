2f868bb923f30c4033adf37c8f259540c74ef1e0
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-10 12:10:21 -0700

    Frontend: Add toggle state

--- src/components/02-Toggle.jsx
+++ src/components/02-Toggle.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
+
 export default function Toggle() {
-  return (
-    <div className="App">
-      <h2>Toggle</h2>
-      <div className="emoji">💡 🌌</div>
-      <div className="button-group">
-        <button>Day</button>
-        <button>Night</button>
-        <button>Toggle</button>
-      </div>
-    </div>
-  )
+    const [emoji, setEmoji] = useState(false)
+
+    const onToggle = () => {setEmoji(prev => !prev)}
+    
+    return (
+        <div className="App">
+            <h2>Toggle</h2>
+            <div className="emoji">{emoji ? "💡" : "🌌"}</div>
+            <div className="button-group">
+                <button onClick={() => {setEmoji(true)}}>Day</button>
+                <button onClick={() => {setEmoji(false)}}>Night</button>
+                <button onClick={onToggle}>Toggle</button>
+            </div>
+        </div>
+    );
 }

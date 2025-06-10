600882820e3d985f9eab2d81ce74c1dc798336aa
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-10 12:10:13 -0700

    Frontend: Add Counter state

--- src/components/01-Counter.jsx
+++ src/components/01-Counter.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 export default function Counter() {
-  return (
-    <div>
-      <h2>Counter</h2>
-      <h3>7</h3>
-      <div className="button-group">
-        <button>Increase</button>
-        <button>Decrease</button>
-      </div>
-    </div>
-  )
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <h2>Counter</h2>
+            <h3>{counter}</h3>
+            <div className="button-group">
+                <button
+                    onClick={() => {
+                        setCounter((prev) => prev + 1);
+                    }}
+                >
+                    Increase
+                </button>
+                <button
+                    onClick={() => {
+                        setCounter((prev) => prev - 1);
+                    }}
+                >
+                    Decrease
+                </button>
+            </div>
+        </div>
+    );
 }
